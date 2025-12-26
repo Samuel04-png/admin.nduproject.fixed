1eff1aac4894d4f86a3c241a29812c1aca86ef5e
Samuel kamanga <skamanga85@gmail.com>
2025-12-26 17:20:37 +0200

    Delete Downloads/Ndu_Project-main (2)/Ndu_Project-mainadmin12242025 directory

--- Downloads/Ndu_Project-main (2)/Ndu_Project-mainadmin12242025/lib/services/google_sign_in_adapter.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-// Conditional import: Web uses popup; others use google_sign_in plugin
-import 'google_sign_in_adapter_web.dart' if (dart.library.io) 'google_sign_in_adapter_io.dart' as impl;
-
-Future<UserCredential> signIn() => impl.signIn();
--- Downloads/Ndu_Project-main (2)/Ndu_Project-mainadmin12242025/lib/services/google_sign_in_adapter_web.dart
+++
@@ -1,6 +0,0 @@
-import 'package:firebase_auth/firebase_auth.dart';
-
-Future<UserCredential> signIn() async {
-  final provider = GoogleAuthProvider();
-  return await FirebaseAuth.instance.signInWithPopup(provider);
-}
